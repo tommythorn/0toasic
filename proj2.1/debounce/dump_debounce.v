990935e3052e75b1c8814a18046a43c1c4727404
Tommy Thorn <tommy-github2@thorn.ws>
2021-02-20 23:31:36 -0800

    Finish migration

--- proj2.1/debounce/dump_debounce.v
+++
@@ -1,9 +0,0 @@
-// Yes, I stole this also.  I have no shame.
-`timescale 1ns/10ps
-module dump();
-    initial begin
-        $dumpfile ("debounce.vcd");
-        $dumpvars (0, debounce);
-        #1;
-    end
-endmodule
